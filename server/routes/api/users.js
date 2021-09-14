e2675460686c2d946d679fb8fd83b6c388bc8db2
Matthew Slater <slaterm100@gmail.com>
2021-09-13 20:42:32 -0400

    with validation, working

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -6,18 +6,18 @@ const jwt = require("jsonwebtoken");
 const passport = require("passport");
 
 // Load input validation
-// const validateRegisterInput = require("../../validation/register");
-// const validateLoginInput = require("../../validation/login");
-// Load User model
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+//Load User model;
 const User = require("../../models/User");
 
 router.post("/register", (req, res) => {
   // Form validation
-// const { errors, isValid } = validateRegisterInput(req.body);
-// // Check validation
-//   if (!isValid) {
-//     return res.status(400).json(errors);
-//   }
+const { errors, isValid } = validateRegisterInput(req.body);
+// Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
