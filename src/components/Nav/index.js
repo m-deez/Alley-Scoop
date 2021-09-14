a654410e830f3c845961ab56580e178822bfe4f7
Mehari Abraha <mehariogbe@yahoo.com>
2021-09-13 19:22:01 -0500

    add nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./styles.css";
+
+
+class Nav extends React.Component {
+    render() {
+        return (
+        <div className="nav">
+            <h1>Home</h1>
+            <h1>Profile</h1>
+            <h1>Log out</h1>
+        </div>
+   )
+}
+     
+   
+}
+   
+
+export default Nav
