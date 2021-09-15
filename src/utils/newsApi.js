a19c52e9309b42507485c8aa6000c1d894a3fdef
Matthew Slater <slaterm100@gmail.com>
2021-09-14 22:44:04 -0400

    fix loop

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -9,11 +9,12 @@ export const NewsContextProvider = (props) => {
     //"4a58b460765c477aabdc11d5ad2c4eac" old key timed out
     
     useEffect(() =>{
-axios.get(
-`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${apiKey}`).then(response => setData(response.data)).catch(err => console.log(err)); 
-console.log(data);
-    },[data]);
-    
+        axios.get(
+            `https://newsapi.org/v2/top-headlines?country=us&category=sports&pageSize=20&apiKey=${apiKey}`).then(response => setData(response.data)).catch(err => console.log(err)); 
+            //console.log(data);
+},[]);
+
+
     return(
         <NewsContext.Provider value={{data}}>
         {props.children}
