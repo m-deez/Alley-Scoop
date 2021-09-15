15e702774db324805135253617ad715f0c42a8e4
Matthew Slater <slaterm100@gmail.com>
2021-09-14 23:42:56 -0400

    rename links

--- src/components/news-api/NewsArticle.js
+++ src/components/news-api/NewsArticle.js
@@ -7,10 +7,8 @@ function NewsArticle({data}) {
       <h6 className="news__title">{data.title}</h6>
       <p className="news__desc">{data.description}</p>
       <span className="news__author">{data.author}</span> <br />
-      {/* <span className="news__published">{data.publishedAt}</span> */}
-      {/* <img src={data.urlToImage} alt="picture" /> */}
       <span className="news__source">{data.source.name}</span>
-      <a href ={data.url} target="_blank">Click Here</a>
+      <a href ={data.url} target="_blank">Link Here</a>
     </div>
     );
 }
