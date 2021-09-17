cf205d80e036cf3e76756528779dfc1737d5f53a
Matt DeZarn <matt.dezarn@gmail.com>
2021-09-17 14:49:00 -0700

    changed base_url

--- src/api/connection.js
+++ src/api/connection.js
@@ -3,8 +3,8 @@ import axios from "axios";
 console.log("NODE ENV WHAT DO YOU MEAN?", process.env.NODE_ENV);
 
 export default axios.create({
-    // baseURL: "https://vast-brushlands-52387.herokuapp.com/api",
-    baseURL: `http://localhost:${process.env.PORT}/api`,
+    baseURL: "https://vast-brushlands-52387.herokuapp.com/api",
+    // baseURL: `http://localhost:${process.env.PORT}/api`,
     headers: {
         "Content-type": "application/json",
     },
