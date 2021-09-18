5de71d6a61feb81b5c38086a484bfcd4ee4d9719
Matthew Slater <slaterm100@gmail.com>
2021-09-18 00:18:12 -0400

    add jordan image fromnt page

--- src/utils/newsApi.js
+++ src/utils/newsApi.js
@@ -7,7 +7,7 @@ export const NewsContextProvider = (props) => {
     const [data, setData] = useState()
     const apiKey = "4a58b460765c477aabdc11d5ad2c4eac"
     // old key timed out
-    // "48c0314982244889b524282371ce22c5"z
+    // "48c0314982244889b524282371ce22c5"
     
     useEffect(() =>{
         axios.get(
@@ -21,11 +21,4 @@ export const NewsContextProvider = (props) => {
         {props.children}
         </NewsContext.Provider>
     );
-};//Allow origin access origin and methods 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-  next();
-});
+};
