6c7c247c3730957232469bc024c8ee110333e091
Matt DeZarn <matt.dezarn@gmail.com>
2021-09-17 23:39:13 -0700

    checking heroku

--- src/api/connection.js
+++ src/api/connection.js
@@ -3,8 +3,8 @@ import axios from "axios";
 console.log("NODE ENV WHAT DO YOU MEAN?", process.env.NODE_ENV);
 
 export default axios.create({
-    // baseURL: "https://vast-brushlands-52387.herokuapp.com/api",
-    baseURL: `http://localhost:5000/api`,
+    baseURL: "https://vast-brushlands-52387.herokuapp.com/api",
+    // baseURL: `http://localhost:5000/api`,
     headers: {
         "Content-type": "application/json",
     },
